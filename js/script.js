d0679070c7a86d255622da37f5b4b0a7fa21eeaa
Abdurahim Abdurahmonov <103033767+abdurahmonov-abdurahim@users.noreply.github.com>
2023-03-10 16:16:32 +0500

    Second Work

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const arrfirst = [1,-4,7,12]
+
+function positiveSum(arr) {
+  arr.forEach(function calcPositive(positive) {
+
+let a = 2
+
+    if (0 < positive){
+      console.log(a + positive);
+    }
+
+  })
+
+}
+
+(positiveSum(arrfirst));
